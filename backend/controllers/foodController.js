5d92cb5ec9bf12507ee13e9a243bfea7dd6bcca0
Ramla Eman <ramla.eman.ra@gmail.com>
2025-07-03 16:37:02 +0500

    completed the backend

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -3,7 +3,50 @@ import fs from "fs";
 
 // add food item
 const addFood = async (req, res) => {
-    
-}
+  let image_filename = `${req.file.filename}`;
 
-export {addFood}
+  const food = new foodModel({
+    name: req.body.name,
+    discription: req.body.discription,
+    price: req.body.price,
+    category: req.body.category,
+    image: image_filename,
+  });
+  try {
+    await food.save();
+    res.json({ success: true, message: "Food item added successfully" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: `Failed to add food item ${error}` });
+  }
+};
+
+// all food list
+const listFood = async (req, res) => {
+  try {
+    const foods = await foodModel.find({});
+    res.json({ success: true, data: foods });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: `Failed to get food list ${error}` });
+  }
+};
+
+// remove food item
+const removeFood = async (req, res) => {
+  try {
+    const food = await foodModel.findById(req.body.id);
+    fs.unlink(`upload/${food.image}`, () => {});
+
+    await foodModel.findByIdAndDelete(req.body.id);
+    res.json({ success: true, message: "Food item removed successfully" });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: `Failed to remove food item ${error}`,
+    });
+  }
+};
+
+export { addFood, listFood, removeFood };
