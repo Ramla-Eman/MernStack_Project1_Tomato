c424b51650a3b9c82483405cf41f2bdfd154c8d4
Ramla Eman <ramla.eman.ra@gmail.com>
2025-07-06 10:26:36 +0500

    set the backend url

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,6 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 
 const App = () => {
+  const url = 'http://localhost:4500'
   return (
     <div>
       <ToastContainer/>
@@ -18,9 +19,9 @@ const App = () => {
       <div className='flex'>
         <Sidebar/>
         <Routes>
-          <Route path='/add' element={<Add/>}/>
-          <Route path='/list' element={<List/>}/>
-          <Route path='/order' element={<Order/>}/>
+          <Route path='/add' element={<Add url={url}/>}/>
+          <Route path='/list' element={<List url={url}/>}/>
+          <Route path='/order' element={<Order url={url}/>}/>
         </Routes>
       </div>
     </div>
