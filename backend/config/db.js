acad2ebc777917d05b728442d6fbd8fb23c398c8
Ramla Eman <ramla.eman.ra@gmail.com>
2025-07-18 15:31:03 +0500

    trying to deplloy backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose
     .connect(
-      "mongodb+srv://ramlaemanra:OcyeZ3YxzJXjlJfF@cluster0.yeu3p1r.mongodb.net/food-del"
+      process.env.MONGO_URI
     )
     .then(() => {
       console.log("DB is Connected");
--- backend/server.js
+++ backend/server.js
@@ -32,3 +32,5 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port http://localhost:${port}`);
 });
+
+export default app;
