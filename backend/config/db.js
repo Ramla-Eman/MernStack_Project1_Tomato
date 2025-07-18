bf951e40b62d99918300697b14f1bfb0d3abe64b
Ramla Eman <ramla.eman.ra@gmail.com>
2025-07-18 15:50:20 +0500

    working on deployement

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  await mongoose
-    .connect(
-      process.env.MONGO_URI
-    )
-    .then(() => {
-      console.log("DB is Connected");
-    });
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("DB is Connected");
+  } catch (error) {
+    console.error("DB connection failed:", error);
+    // Exit process with failure
+    process.exit(1);
+  }
 };
